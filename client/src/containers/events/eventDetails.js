35b11e743d23ee9f99512de4c6cb37d4060d43a5
Shi-Hao Hong <shihaohong94@gmail.com>
2017-07-27 16:39:01 -0700

    implement google maps feature for specific events
    * implement google maps feature for specific events
    
    * remove unneccessary comments from eventDetails.js

--- client/src/containers/events/eventDetails.js
+++ client/src/containers/events/eventDetails.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchChannels } from '../../actions';
 import _ from 'lodash';
+// import geocoder from 'geocoder';
 
 import { Segment, Icon } from 'semantic-ui-react';
 
@@ -9,6 +10,34 @@ export class EventDetails extends Component {
   constructor(props) {
     super(props);
     console.log('event details rendered. event id: ', this.props.eventId);
+  }
+
+  componentDidMount() {
+    this.renderMaps();
+  }
+
+  renderMaps() {
+    let map, marker;
+    let address = this.props.events[this.props.eventId].address;
+    let geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+        let mapOptions = {
+          center: results[0].geometry.location,
+          zoom: 15
+        };
+        
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        let marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('geocode unsuccessful: ', status);
+      }
+    });
   }
 
   render() {
@@ -19,6 +48,7 @@ export class EventDetails extends Component {
         Name: {currentEvent.name} <br/>
         Date: {currentEvent.date} <br/>
         Address: {currentEvent.address} <br/>
+        <div id='map-canvas'></div>
         Time: {currentEvent.time} <br/>
       </div>
     );
