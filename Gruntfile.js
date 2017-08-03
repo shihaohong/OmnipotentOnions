fec8bc616ca8132f83a74b8bda10a8ea95bd79fa
Shi-Hao Hong <shihaohong94@gmail.com>
2017-08-03 16:55:17 -0700

    update documentation and delete unnecessary config json files

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
           password: config.connection.password,
           host: config.connection.host,
           port: config.connection.port,
-          database: 'template1'
+          database: 'thesis'
         },
         name: config.connection.database
       }
